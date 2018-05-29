59ade26041be2d144c42aa05c72bb4a3310d77f8
hezhihao <hzhandhyx@163.com>
2018-05-29 09:34:38 +0800

    123123

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -1,79 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name='csrf-token' content="{{ csrf_token() }}">
-	<title>Document</title>
-	<link rel="stylesheet" href="{{asset('css/normalize.css')}}" type="text/css" />
-	<link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}" type="text/css" />
-	<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}"type="text/css"/>
-	<link rel="stylesheet" href="{{asset('css/index.css')}}" type="text/css" />
-	<link rel="stylesheet" href="{{asset('bootstrap/bootstrap-3.3.7-dist/css/bootstrap.min.css')}}">
-	<style>
-		
-	</style>
+    <meta charset="UTF-8">
+    <meta name='csrf-token' content="{{ csrf_token() }}">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/normalize.css')}}" type="text/css" />
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}" type="text/css" />
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}"type="text/css"/>
+    <link rel="stylesheet" href="{{asset('css/index.css')}}" type="text/css" />
+    <link rel="stylesheet" href="{{asset('bootstrap/bootstrap-3.3.7-dist/css/bootstrap.min.css')}}">
+    <style>
+        
+    </style>
 @section('css')
 
 @show
 </head>
 <body>
-	<div id="app">
-		
-	</div>
-	<!--头部导航栏开始-->
-	<div class="header-nav">
-		<div class="header-wrap">
-			<a class="mgj-home" href="{{action('IndexController@index')}}">蘑菇街首页</a>
-			<ul class="header-wrap-nav">
-				<li class="login header-item">
-					<a href="#">注册</a>
-					<a href="#" id="login">登录</a>
-				</li>
-				<li class="order header-item">
-					<a href="#">我的订单</a>
-				</li>
-				<li class="cart header-item"id="cart">
-					<a href="{{action('ShoppingController@index')}}">购物车</a>
-					<div class="empty-cart">购物车里没有商品</div>
-					
-				</li>
-				<li class="custom header-item" id="custom">
-					<a href="#">客户服务</a>
-					<i class="icon_delta"></i>
-					<ol class="custom-con">
-						<li class="cs2">
-							<a href="#">消费者服务</a>
-						</li>
-						<li class="cs2">
-							<a href="#">商家服务</a>
-						</li>
-						<li class="cs2">
-							<a href="#">规则中心</a>
-						</li>
-					</ol>
-				</li>
-				<li class="store header-item"id="store">
-					<a href="#">我的小店</a>
-					<ol class="store-con">
-						<li class="s2">
-							<a href="#">管理后台</a>
-						</li>
-						<li class="s2">
-							<a href="#">商家社区</a>
-						</li>
-						<li class="s2">
-							<a href="#">商家培训</a>
-						</li>
-						<li class="s2">
-							<a href="#">市场入驻</a>
-						</li>
-					</ol>
-				</li>
-			</ul>
-		</div>
-	</div>
-	<!--头部导航栏结束-->
-	 @section('content')
+    <div id="app">
+        
+    </div>
+    <!--头部导航栏开始-->
+    <div class="header-nav">
+        <div class="header-wrap">
+            <a class="mgj-home" href="{{action('IndexController@index')}}">蘑菇街首页</a>
+            <ul class="header-wrap-nav">
+                <li class="login header-item">
+                    <a href="#">注册</a>
+                    <a href="#" id="login">登录</a>
+                </li>
+                <li class="order header-item">
+                    <a href="#">我的订单</a>
+                </li>
+                <li class="cart header-item"id="cart">
+                    <a href="{{action('ShoppingController@index')}}">购物车</a>
+                    <div class="empty-cart">购物车里没有商品</div>
+                    
+                </li>
+                <li class="custom header-item" id="custom">
+                    <a href="#">客户服务</a>
+                    <i class="icon_delta"></i>
+                    <ol class="custom-con">
+                        <li class="cs2">
+                            <a href="#">消费者服务</a>
+                        </li>
+                        <li class="cs2">
+                            <a href="#">商家服务</a>
+                        </li>
+                        <li class="cs2">
+                            <a href="#">规则中心</a>
+                        </li>
+                    </ol>
+                </li>
+                <li class="store header-item"id="store">
+                    <a href="#">我的小店</a>
+                    <ol class="store-con">
+                        <li class="s2">
+                            <a href="#">管理后台</a>
+                        </li>
+                        <li class="s2">
+                            <a href="#">商家社区</a>
+                        </li>
+                        <li class="s2">
+                            <a href="#">商家培训</a>
+                        </li>
+                        <li class="s2">
+                            <a href="#">市场入驻</a>
+                        </li>
+                    </ol>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!--头部导航栏结束-->
+     @section('content')
         
     @show
 </body>
--- blog/storage/framework/views/768c199da7157e942c480214ed72ffe3ca313930.php
+++ blog/storage/framework/views/768c199da7157e942c480214ed72ffe3ca313930.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">登录</div>
+                <div class="panel-heading">Login</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="<?php echo e(route('login')); ?>">
@@ -11,7 +11,7 @@
 
 
                         <div class="form-group<?php echo e($errors->has('email') ? ' has-error' : ''); ?>">
-                            <label for="email" class="col-md-4 control-label">邮箱</label>
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="<?php echo e(old('email')); ?>" required autofocus>
@@ -25,7 +25,7 @@
                         </div>
 
                         <div class="form-group<?php echo e($errors->has('password') ? ' has-error' : ''); ?>">
-                            <label for="password" class="col-md-4 control-label">密码</label>
+                            <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -42,7 +42,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>> 记住密码
+                                        <input type="checkbox" name="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>> Remember Me
                                     </label>
                                 </div>
                             </div>
@@ -51,11 +51,11 @@
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    登录
+                                    Login
                                 </button>
 
                                 <a class="btn btn-link" href="<?php echo e(route('password.request')); ?>">
-                                    忘记密码/登录遇到问题？
+                                    Forgot Your Password?
                                 </a>
                             </div>
                         </div>
